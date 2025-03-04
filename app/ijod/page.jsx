d32a7244f111ffee8c5bf383de8a6e05a3c3b5fa
ziyodyakubov <ziyodyakubov23@gmail.com>
2025-03-05 01:00:31 +0500

    fixed some

--- app/ijod/page.jsx
+++ app/ijod/page.jsx
@@ -10,7 +10,7 @@ const News = () => {
     }, [])
     return (
         <>
-            <main className="flex flex-col justify-center items-center pt-[30px]">
+            <main className="flex nimadir flex-col justify-center items-center pt-[30px]">
                 <iframe
                     className="rounded-[12px] mb-[30px] w-[370px] lg:w-[1280px] h-[720px]"
                     src="https://open.spotify.com/embed/artist/3sxuSPGJrQXReGDnPAc9ST?utm_source=generator&theme=0"
