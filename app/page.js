55f161a10b60d0d16d3c3acc83b14740fdf75b86
ziyodyakubov <ziyodyakubov23@gmail.com>
2025-03-06 12:17:37 +0500

    fixed color

--- app/page.js
+++ app/page.js
@@ -22,32 +22,32 @@ const Layout = () => {
 
         <div className="h-[370px] lg:flex hidden flex-col items-center justify-center bg-[#70372A]">
           <div className="grid grid-cols-[auto_1px_auto_1px_auto_1px_auto_1px_auto_1px_auto] pb-[150px] items-center gap-[50px]">
-            <a href="https://music.yandex.ru/artist/10747621?utm_medium=copy_link" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://music.yandex.ru/artist/10747621?utm_medium=copy_link" target="_blank">
               <Image className="w-[95px] rounded-[10px] relative left-[40px]" src={YandexMusic} alt="Yandex Music" />
             </a>
             <div className="w-[1px] h-[130px] ml-[40px] bg-[#8A8A8A]"></div>
 
-            <a href="https://music.apple.com/uz/artist/shokir/1549932788" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://music.apple.com/uz/artist/shokir/1549932788" target="_blank">
               <Image className="w-[125px] relative left-[25px]" src={AppleMusic} alt="Apple Music" />
             </a>
             <div className="w-[1px] ml-[15px] h-[130px] bg-[#8A8A8A]"></div>
 
-            <a href="https://open.spotify.com/artist/3sxuSPGJrQXReGDnPAc9ST?si=4FdJWWNdTceeUNKG8tClQg&nd=1&dlsi=4fcfc5f6663548ba" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://open.spotify.com/artist/3sxuSPGJrQXReGDnPAc9ST?si=4FdJWWNdTceeUNKG8tClQg&nd=1&dlsi=4fcfc5f6663548ba" target="_blank">
               <Image className="w-[140px]" src={Spotify} alt="Spotify" />
             </a>
             <div className="w-[1px] h-[130px] ml-[-20px] bg-[#8A8A8A]"></div>
 
-            <a href="https://www.instagram.com/millymallymoe" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://www.instagram.com/millymallymoe" target="_blank">
               <Image className="w-[100px] relative right-[15px]" src={Insta} alt="Instagram" />
             </a>
             <div className="w-[1px] h-[130px] ml-[-15px] bg-[#8A8A8A]"></div>
 
-            <a href="https://t.me/therealmilly" className="grayscale-[60%] hover:brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://t.me/therealmilly" target="_blank">
               <Image className="w-[100px] relative right-[15px]" src={Telegram} alt="Telegram" />
             </a>
             <div className="w-[1px] h-[130px] ml-[-15px] bg-[#8A8A8A]"></div>
 
-            <a href="https://www.youtube.com/c/MILLYMALLYMOE" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+            <a href="https://www.youtube.com/c/MILLYMALLYMOE" target="_blank">
               <Image className="w-[105px] relative right-[10px]" src={YouTube} alt="YouTube" />
             </a>
           </div>
@@ -60,31 +60,31 @@ const Layout = () => {
         <div className="h-[570px] lg:hidden flex flex-col items-center px-[20px] justify-center bg-[#70372A]">
           <div className="lg:pb-[150px] pb-[100px] px-[10px] justify-center flex-col flex items-center gap-[25px]">
             <div className="flex gap-[70px] pl-[0px] items-center">
-              <a href="https://music.yandex.ru/artist/10747621?utm_medium=copy_link" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://music.yandex.ru/artist/10747621?utm_medium=copy_link" target="_blank">
                 <Image className="lg:w-[95px] w-[80px] rounded-[10px] relative" src={YandexMusic} alt="Yandex Music" />
               </a>
 
-              <a href="https://music.apple.com/uz/artist/shokir/1549932788" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://music.apple.com/uz/artist/shokir/1549932788" target="_blank">
                 <Image className="lg:w-[125px] w-[105px] relative" src={AppleMusic} alt="Apple Music" />
               </a>
             </div>
 
             <div className="flex gap-[60px] items-center pr-[25px]">
-              <a href="https://open.spotify.com/artist/3sxuSPGJrQXReGDnPAc9ST?si=4FdJWWNdTceeUNKG8tClQg&nd=1&dlsi=4fcfc5f6663548ba" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://open.spotify.com/artist/3sxuSPGJrQXReGDnPAc9ST?si=4FdJWWNdTceeUNKG8tClQg&nd=1&dlsi=4fcfc5f6663548ba" target="_blank">
                 <Image className="lg:w-[140px] w-[120px]" src={Spotify} alt="Spotify" />
               </a>
 
-              <a href="https://www.instagram.com/millymallymoe" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://www.instagram.com/millymallymoe" target="_blank">
                 <Image className="lg:w-[100px] w-[80px] relative lg:right-[15px]" src={Insta} alt="Instagram" />
               </a>
             </div>
 
             <div className="flex gap-[75px] items-center">
-              <a href="https://t.me/therealmilly" className="grayscale-[60%] hover:brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://t.me/therealmilly" target="_blank">
                 <Image className="lg:w-[100px] w-[85px] relative lg:right-[15px]" src={Telegram} alt="Telegram" />
               </a>
 
-              <a href="https://www.youtube.com/c/MILLYMALLYMOE" className="grayscale-[60%] brightness-100 hover:grayscale-[0%]" target="_blank">
+              <a href="https://www.youtube.com/c/MILLYMALLYMOE" target="_blank">
                 <Image className="lg:w-[105px] w-[90px] relative lg:right-[10px]" src={YouTube} alt="YouTube" />
               </a>
             </div>
